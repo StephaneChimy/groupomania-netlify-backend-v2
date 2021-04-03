a29a6d93a55c26442a8ef9da4799e23bc703f6cb
Stéphane Chimy <stephane.akira@gmail.com>
2021-04-03 11:50:15 +0200

    configure server.js

--- express/server.js
+++ express/server.js
@@ -14,6 +14,17 @@ const xss = require('xss-clean');
 const router = express.Router();
 const app = express();
 
+router.get('/', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1>Hello from Express.js!</h1>');
+  res.end();
+});
+router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
+router.post('/', (req, res) => res.json({ postBody: req.body }));
+
+app.use('/.netlify/functions/server', router);  // path must route to lambda
+app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+
 app.use(helmet());
 app.use(cors({origin:["https://sc-groupomania.netlify.app", "http://localhost:3001", "https://sc-groupomania-backend.herokuapp.com/"], credentials: true}));
 //app.use(cors());
@@ -25,16 +36,7 @@ app.use(xss());
 app.use(express.json({ limit: '10kb' })); // Body limit is 10kb
 
 
-router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
-});
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
 
-app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
 
 // app.use("/api/auth", userRoutes);
