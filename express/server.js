e922ad05dfeb42047bba74e20ee9c34f678d9ae7
Stéphane Chimy <stephane.akira@gmail.com>
2021-04-03 12:05:44 +0200

    Configure dependences server.js

--- express/server.js
+++ express/server.js
@@ -8,9 +8,9 @@ const bodyParser = require('body-parser');
 const cookieParser = require("cookie-parser");
 // const userRoutes = require("./routes/user");
 // const messageRoutes = require("./routes/message");
-// const cors = require('cors');
-// const helmet = require('helmet');
-// const xss = require('xss-clean');
+const cors = require('cors');
+const helmet = require('helmet');
+const xss = require('xss-clean');
 
 const router = express.Router();
 router.get('/', (req, res) => {
